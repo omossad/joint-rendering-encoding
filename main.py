ce6ea98f9c2b6611e38d492cb5753ff7e6048211
Omar Mossad <omar.salaheldine@gmail.com>
2022-03-22 20:16:51 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def readNetworkTraces():
+    return
+
+def encodeVideo():
+    return
+
+def calculateVMAF():
+    return
+
+def navigateToFrame():
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
